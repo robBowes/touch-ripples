1970f68636d4d67ca86030b947af70312f8948bb
Robert Bowes <r.e.bowes@gmail.com>
2019-04-03 14:58:30 -0400

    add source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const webpack = require("webpack")
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
 
 /*
  * SplitChunksPlugin is enabled by default and replaced
@@ -25,80 +24,79 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
  *
  */
 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				include: [path.resolve(__dirname, 'src')],
-				loader: 'babel-loader',
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, "src")],
+        loader: "babel-loader",
+        options: {
+          plugins: ["syntax-dynamic-import"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                modules: false
+              }
+            ]
+          ]
+        },
+        test: /\.js$/
+      },
+      {
+        test: /\.(scss|css)$/,
 
-				options: {
-					plugins: ['syntax-dynamic-import'],
-
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								modules: false
-							}
-						]
-					]
-				},
-
-				test: /\.js$/
-			},
-			{
-				test: /\.(scss|css)$/,
-
-				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader'
-					},
-					{
-						loader: 'sass-loader'
-					}
-				]
-			}
-		]
-	},
-
-	output: {
-		chunkFilename: '[name].[chunkhash].js',
-		filename: '[name].[chunkhash].js'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "Ripples",
-			inject: false,
-			mobile: true,
-			scripts: ["https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.min.js", "https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.12.0/matter.min.js"],
-			template: "./src/template.html",
-		}),
-		new CleanWebpackPlugin(),
-	],	
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
-
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	},
-	devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    chunkFilename: "[name].[chunkhash].js",
+    filename: "[name].[chunkhash].js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Ripples",
+      inject: false,
+      mobile: true,
+      scripts: [
+        "https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.12.0/matter.min.js"
+      ],
+      template: "./src/template.html"
+    }),
+    new CleanWebpackPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+      chunks: "async",
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 8000
-  }
-};
+	},
+	devtool: "source-map"
+}
