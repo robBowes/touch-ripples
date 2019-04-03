2c09eb29e813dfdaabfd3ba7c2bdd9b22676ea83
Robert Bowes <r.e.bowes@gmail.com>
2019-04-03 14:57:43 -0400

    filter out ripples that don't exist

--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
-import Ripple from './ripple';
+import Ripple from "./ripple"
 
 const canvas = document.querySelector("#sketch")
 const canvasAttrs = canvas.getBoundingClientRect()
 const HEIGHT = canvasAttrs.height
 const WIDTH = canvasAttrs.width
 
-
 function sketch(p5) {
-    const engine = Matter.Engine.create()
-    let ripples = []
-    p5.setup = function() {
-        p5.createCanvas(WIDTH, HEIGHT)
-        p5.background(0)
-        p5.frameRate(25)
-        Matter.Engine.run(engine)
-      }
-      
-    p5.draw = function() {
-      p5.background(0)
-      // Check if any ripples don't exist, we don't need to check every frame, so check every 4s
-      if (p5.frameCount % 100) {
-        ripples = ripples.filter(ripple => ripple.exists)
-      }
-      ripples.map(ripple => Ripple.update(ripple, p5.millis())).forEach(ripple => Ripple.render(p5, ripple))
-    }
+  const engine = Matter.Engine.create()
+  let ripples = []
+  p5.setup = function() {
+    p5.createCanvas(WIDTH, HEIGHT)
+    p5.background(0)
+    p5.frameRate(25)
+    Matter.Engine.run(engine)
+  }
 
-    p5.mousePressed = function() {
-      ripples.push(new Ripple(p5.millis(), {x: p5.mouseX, y: p5.mouseY}))
+  p5.draw = function() {
+    p5.background(0)
+    // Check if any ripples don't exist, we don't need to check every frame, so check every 4s
+    if (p5.frameCount % 100) {
+      ripples = ripples.filter(ripple => ripple.exists(p5.millis()))
     }
+    ripples
+      // Create an array of objects from the ripples
+      .map(ripple => Ripple.update(ripple, p5.millis()))
+      // Render the objects using p5
+      .forEach(ripple => Ripple.render(p5, ripple))
+  }
+
+  p5.mousePressed = function() {
+    ripples.push(new Ripple(p5.millis(), { x: p5.mouseX, y: p5.mouseY }))
+  }
 }
 
 const p5Canvas = new p5(sketch, canvas)
--- src/ripple.js
+++ src/ripple.js
@@ -5,18 +5,22 @@ export default class Ripple {
     this.outline = 255
     this.creationTime = creationTime
     this.location = location
-    this.exists = true
   }
 
   static update(ripple, time) {
     const size = Math.floor((time - ripple.creationTime) / 10)
     const outlineColor = fade(ripple.outline, size)
-    return {size, outlineColor, location: ripple.location}
+    return { size, outlineColor, location: ripple.location }
   }
 
   static render(p5, ripple) {
     p5.stroke(ripple.outlineColor)
-    p5.fill(0,0,0,0)
-    p5.ellipse(ripple.location.x,ripple.location.y,ripple.size,ripple.size)
+    p5.fill(0, 0, 0, 0)
+    p5.ellipse(ripple.location.x, ripple.location.y, ripple.size, ripple.size)
+  }
+
+  // Determines if a ripple still exists
+  exists(time) {
+    return time - this.creationTime < 3000
   }
 }
